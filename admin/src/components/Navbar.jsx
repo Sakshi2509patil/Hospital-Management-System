775a452f1091bd504d986996febe35821ae581ba
Sakshi <153505034+Sakshi2509patil@users.noreply.github.com>
2025-08-11 21:49:47 +0530

    Update Navbar.jsx

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,30 +1,3 @@
-// import React, { useContext } from 'react'
-// import { assets } from '../assets/assets'
-// import { AdminContext } from '../context/AdminContext'
-
-// const Navbar = () => {
-
-//     const {aToken , setAToken}=useContext(AdminContext)
-
-//     const logout=()=>{
-//         aToken && setAToken('')
-//         aToken && localStorage.removeItem('aToken')
-//     }
-//   return (
-//     <div>
-//         <div>
-//       <img src={assets.admin_logo} alt="" />
-//       <p>{aToken?'Admin':'Doctor'}</p>
-//     </div>
-//     <button onClick={logout}>
-//         Logout
-//     </button>
-//     </div>
-//   )
-// }
-
-// export default Navbar
-
 import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { AdminContext } from '../context/AdminContext'
