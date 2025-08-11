a55f39489be333fa082a3792c44a91a5b2662f43
Sakshi <153505034+Sakshi2509patil@users.noreply.github.com>
2025-08-11 21:58:09 +0530

    Update mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,30 +1,3 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//     try {
-//         mongoose.connection.on('connected', () => console.log("✅ Database connected"));
-//         await mongoose.connect(process.env.MONGODB_URI);
-//     } catch (error) {
-//         console.error("❌ MongoDB connection error:", error);
-//         process.exit(1);
-//     }
-// };
-
-// export default connectDB;
-
-
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGODB_URI);
-//         console.log("✅ Database connected to:", mongoose.connection.name);
-//     } catch (error) {
-//         console.error("❌ MongoDB connection error:", error.message);
-//     }
-// };
-
-// export default connectDB;
 
 import mongoose from "mongoose";
 
@@ -32,10 +5,10 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
         });
-        console.log(`✅ MongoDB connected: ${conn.connection.host}/${conn.connection.name}`);
+        console.log(` MongoDB connected: ${conn.connection.host}/${conn.connection.name}`);
     } catch (error) {
-        console.error("❌ MongoDB connection error:", error.message);
-        process.exit(1); // Exit process on failure
+        console.error(" MongoDB connection error:", error.message);
+        process.exit(1); 
     }
 };
 
