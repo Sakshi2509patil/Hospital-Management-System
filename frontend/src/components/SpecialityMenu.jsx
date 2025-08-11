9060d1607ea257051d54bcd34811a4012d90f756
Sakshi <153505034+Sakshi2509patil@users.noreply.github.com>
2025-08-11 22:10:54 +0530

    Update SpecialityMenu.jsx

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -1,25 +1,3 @@
-// import React from 'react'
-// import {specialityData} from '../assets/assets'
-// import { Link } from 'react-router-dom'
-// const SpecialityMenu = () => {
-//   return (
-//     <div id='speciality'>
-//       <h1>Find by Speciality</h1>
-//       <p>Explore top-rated doctors and schedule appointments at your convenience.</p>
-//       <div>
-//         {specialityData.map((item,index)=>(
-//            <Link key={index} to={'/doctors/${item.speciality}'}>
-//                <img src={item.image} alt="" />
-//                <p>{item.speciality}</p>
-//            </Link>
-//         ))}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default SpecialityMenu
-
 import React from 'react'
 import { specialityData } from '../assets/assets'
 import { Link } from 'react-router-dom'
